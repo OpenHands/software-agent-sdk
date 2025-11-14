3b1ed86d59798ad62e853ccce37533b3773f4a76
Xingyao Wang <xingyao@all-hands.dev>
2025-11-14 19:55:54 +0000

    add base classes for critic

---
+++ openhands-sdk/openhands/sdk/critic/base.py
@@ -0,0 +1,36 @@
+import abc
+from typing import ClassVar
+
+from pydantic import BaseModel, Field
+
+from openhands.sdk.event import LLMConvertibleEvent
+
+
+class CriticResult(BaseModel):
+    """A critic result is a score and a message."""
+
+    THRESHOLD: ClassVar[float] = 0.5
+
+    score: float = Field(
+        description="A predicted probability of success between 0 and 1.",
+        ge=0.0,
+        le=1.0,
+    )
+    message: str | None = Field(description="An optional message explaining the score.")
+
+    @property
+    def success(self) -> bool:
+        """Whether the agent is successful."""
+        return self.score >= CriticResult.THRESHOLD
+
+
+class BaseCritic(abc.ABC):
+    """A critic is a function that takes in a list of events,
+    optional git patch, and returns a score about the quality of those events.
+    """
+
+    @abc.abstractmethod
+    def evaluate(
+        self, events: list[LLMConvertibleEvent], git_patch: str | None = None
+    ) -> CriticResult:
+        pass
